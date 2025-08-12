26fdb74e80d203ce9cf5165a1b75252aa0ffd7f6
yaymarius <51022769+yaymarius@users.noreply.github.com>
2025-08-12 15:00:41 +0300

    Make the title configurable via config file apidocs.info.title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8" />
-        <title>Laravel</title>
+        <title>{{config(apidocs.info.title)}}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 
